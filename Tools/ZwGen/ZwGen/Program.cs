3064292306592d270a54d39221cd956f7fd01aef
Naruto Mouri <Mouri_Naruto@outlook.com>
2019-01-12 20:44:28 +0800

    Update from https://github.com/processhacker/phnt.

---
+++ Tools/ZwGen/ZwGen/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ZwGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentFilePath = "G:\\Projects\\ChuyuWindowsInternalAPI\\Tools\\ChuyuWindowsInternalAPI.h";
+
+            string text = File.ReadAllText(currentFilePath);
+
+            Regex regex = new Regex(@"NTSYSCALLAPI[\w\s_]*NTAPI\s*(Nt(\w)*)\(.*?\);", RegexOptions.Compiled | RegexOptions.Singleline);
+            MatchCollection matches;
+
+            matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                string currentName = match.Groups[1].Value;
+                string currentText = match.Value;
+                int currentNameIndex = match.Groups[1].Index - match.Index;
+
+                string newText = currentText + "\r\n\r\n" + currentText.Substring(0, currentNameIndex) + "Zw" + currentText.Substring(currentNameIndex + 2);
+
+                // Make sure we don't add definitions repeatedly.
+                if (text.IndexOf(newText) == -1)
+                {
+                    text = text.Replace(currentText, newText);
+                }
+            }
+
+            File.WriteAllText(currentFilePath, text);
+        }
+    }
+}
