6ac95c7461470884dec8ccd3412cb8b3e7351092
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-10 17:36:48 +0800

    Create Mint\Mint.h.

---
+++ Mint/Mint.h
@@ -0,0 +1,28 @@
+﻿/*
+ * PROJECT:   Mouri's Internal NT API Collections (MINT)
+ * FILE:      Mint.h
+ * PURPOSE:   Definition for the Windows Internal API
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef _MINT_
+#define _MINT_
+
+#ifdef __cplusplus
+#ifdef MINT_USE_SEPARATE_NAMESPACE
+namespace MINT {
+#endif // MINT_USE_SEPARATE_NAMESPACE
+#endif // __cplusplus
+
+
+
+#ifdef __cplusplus
+#ifdef MINT_USE_SEPARATE_NAMESPACE
+}
+#endif // MINT_USE_SEPARATE_NAMESPACE
+#endif // __cplusplus
+
+#endif // !_MINT_
