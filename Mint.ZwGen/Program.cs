965e0a98d40d7d3e9300e1201ab452acfb768b32
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-11 12:15:09 +0800

    Add Mint.ZwGen project.

---
+++ Mint.ZwGen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mint.ZwGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
